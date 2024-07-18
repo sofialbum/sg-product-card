5acf672f572a26e4d788fd88aad7729c455aa132
Sofía González <sofia_1.6@hotmail.com>
2024-07-18 17:30:52 -0300

    Version 0.0.1 lista

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,30 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+// // import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+
+
+// describe('it', () => {
+//   it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     // ReactDOM.render(<Thing />, div);
+//     ReactDOM.unmountComponentAtNode(div);
+//   });
+// });
+
+import * as ReactDOM from 'react-dom/client';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    document.body.appendChild(div);
+    
+    // Para React 18, se utiliza createRoot para renderizar
+    const root = ReactDOM.createRoot(div);
+    // root.render(<Thing />);
+
+    // Para desmontar el componente
+    root.unmount();
+    
+    document.body.removeChild(div);
   });
 });
+
